f7c1a6c3a6109027fcafc0dad1c97e517c0997ed
Alexandr <ak4raulov@yandex.ru>
2021-07-02 16:26:12 +0300

    создал табы и карточки

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//const { on } = require("gulp");
 $(document).ready(function(){
 	$('.carousel__inner').slick({
 	   speed: 500,
@@ -17,4 +18,23 @@ $(document).ready(function(){
 		  },
 		]
 	});
+
+	 $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab-active)', function() {
+    $(this)
+      .addClass('catalog__tab-active').siblings().removeClass('catalog__tab-active')
+      .closest('div.container').find('div.catalog__content').removeClass('catalog__content-active').eq($(this).index()).addClass('catalog__content-active');
+	 });
+
+	function toggleClass(item) {
+			$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content-active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list-active');
+			})
+		});
+	};
+
+	toggleClass('.catalog-item__link');
+	toggleClass('.catalog-item__back');
 });

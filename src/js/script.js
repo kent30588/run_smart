0a22f0cd62150840e8a4a5d0523f17e7dfcb86d6
Alexandr <ak4raulov@yandex.ru>
2021-07-13 19:02:01 +0300

    исправил ошибки добавил плавный скролл

--- src/js/script.js
+++ src/js/script.js
@@ -110,7 +110,7 @@ $(document).ready(function () {
 
 // плавный скролл
 	$("a[href^='#']").click(function(){
-				const _href = $(this).attr("href");
+				var _href = $(this).attr("href");
 				$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 				return false;
 	});

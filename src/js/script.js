56c90cf11244f7029687e5a690c611253ce73336
Alexandr <ak4raulov@yandex.ru>
2021-07-15 14:29:18 +0300

    добавил анимацию библиотека Animate.css

--- src/js/script.js
+++ src/js/script.js
@@ -109,9 +109,11 @@ $(document).ready(function () {
 	});
 
 // плавный скролл
-	$("a[href^='#']").click(function(){
+	$("a[href=#up]").click(function(){
 				var _href = $(this).attr("href");
 				$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
 				return false;
 	});
+
+	 new WOW().init();
 });

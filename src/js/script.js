a8eaaeddb12ae2aa2174567f80bf58b746fb5c41
Alexandr <ak4raulov@yandex.ru>
2021-07-15 19:46:53 +0300

    адаптация

--- src/js/script.js
+++ src/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 	$('.carousel__inner').slick({
 		speed: 500,
 		//adaptiveHeight: true,
-		autoplay: true,
+		autoplay: false,
 		autoplaySpeed: 2000,
 		prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
 		nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
@@ -11,8 +11,8 @@ $(document).ready(function () {
 			{
 				breakpoint: 1024,
 				settings: {
-					dots: true,
-					arrows: false,
+					dots: false,
+					arrows: true,
 					dotsClass: 'slick-dots'
 				}
 			},
